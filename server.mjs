d2693ac91ca0c2a6ccf01e59d10d09a0ad190831
geethabathula <bgeethasupriya.99@gmail.com>
2024-07-18 19:39:59 +0530

    connecting api via render trail3

--- server.mjs
+++ server.mjs
@@ -1,9 +1,11 @@
 import jsonServer from 'json-server';
+import cors from 'cors';
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // Path to your JSON file
 const middlewares = jsonServer.defaults();
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 server.listen(5000, () => {
